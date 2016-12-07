faf01497f0c665662d682f2ed7891c6b0f3283d5
Gekii <stephan.gruenes@hotmail.de>
2016-12-07 11:58:40 +0100

    First File

---
+++ Script.ps1
@@ -0,0 +1,13 @@
+﻿class WER
+{
+	[System.Collections.ArrayList]$werFiles
+}
+
+class WERFile
+{
+	[string]$filename
+	[string]$path
+	[Object]$computer
+	[Object]$user
+	[System.Collections.Hashtable]$content
+}
